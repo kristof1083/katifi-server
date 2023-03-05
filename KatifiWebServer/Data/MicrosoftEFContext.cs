9a960cc188fa5e387cf78e34618f3bc051d2550c
lytwp8 <lytwp8@pte.hu>
2023-03-05 13:49:15 +0100

    Add empty DbContext and set the Connection Strings

---
+++ KatifiWebServer/Data/MicrosoftEFContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+//using KatifiWebServer.Models;
+
+namespace KatifiWebServer.Data
+{
+    public partial class MicrosoftEFContext : DbContext
+    {
+        public MicrosoftEFContext(DbContextOptions<MicrosoftEFContext> options) : base(options) { }
+
+
+
+    }
+}
--- KatifiWebServer/Program.cs
+++ KatifiWebServer/Program.cs
@@ -1,8 +1,14 @@
+using KatifiWebServer.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<MicrosoftEFContext>(opt => opt.UseSqlServer(
+    builder.Configuration.GetConnectionString("LocalConnection")));
 
 var app = builder.Build();
 
