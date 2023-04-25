ed6471c2cbdc83edb1c6a9c94a8b25ff8b053d11
lytwp8 <lytwp8@pte.hu>
2023-04-25 19:39:01 +0200

    Create AddressController.cs and ChurchController.cs

---
+++ KatifiWebServer/Controllers/AddressController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KatifiWebServer.Models.DatabaseModels;
+using AutoMapper;
+using KatifiWebServer.Models.DTOModels;
+using KatifiWebServer.Services;
+
+namespace KatifiWebServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        private readonly IAddressService _service;
+        private readonly IMapper _mapper;
+
+        public AddressController(IMapper mapper, IAddressService service)
+        {
+            _mapper = mapper;
+            _service = service;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AddressDTO>>> GetAddresses()
+        {
+            var addresses = await _service.GetAllAsync();
+            var addressdtos = _mapper.Map<IEnumerable<AddressDTO>>(addresses);
+
+            return Ok(addressdtos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AddressDTO>> GetAddress(int id)
+        {
+            var address = await _service.GetByIdAsync(id);
+
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<AddressDTO>(address));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<AddressDTO>> PutAddress(int id, AddressDTO addressdto)
+        {
+            var address = _mapper.Map<Address>(addressdto);
+            if (id != address.Id || !await _service.MeetsTheConstraints(address))
+            {
+                return BadRequest();
+            }
+
+            await _service.UpdateAsync(address);
+            return Ok(_mapper.Map<AddressDTO>(address));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AddressDTO>> PostAddress(AddressDTO addressdto)
+        {
+            var address = _mapper.Map<Address>(addressdto);
+            if(!await _service.MeetsTheConstraints(address))
+            {
+                return BadRequest();
+            }
+
+            await _service.AddAsync(address);
+            return CreatedAtAction(nameof(GetAddress), new { id = address.Id }, _mapper.Map<AddressDTO>(address));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAddress(int id)
+        {
+            if (!await _service.EntityExists(id))
+            {
+                return NotFound();
+            }
+
+            await _service.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ KatifiWebServer/Controllers/ChurchController.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using KatifiWebServer.Models.DatabaseModels;
+using KatifiWebServer.Models.DTOModels;
+using KatifiWebServer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace KatifiWebServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChurchController : ControllerBase
+    {
+        private readonly IChurchService _service;
+        private readonly IMapper _mapper;
+
+        public ChurchController(IChurchService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ChurchDTO>>> GetChurches()
+        {
+            var churches = await _service.GetAllAsync(c => c.Address, c => c.Messes);
+            var churchdtos = _mapper.Map<IEnumerable<ChurchDTO>>(churches);
+
+            return Ok(churchdtos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ChurchDTO>> GetChurch(int id)
+        {
+            var church = await _service.GetByIdAsync(id, c => c.Address, c => c.Messes);
+
+            if (church == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(church);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ChurchDTO>> PutChurch(int id, ChurchDTO churchdto)
+        {
+            var church = _mapper.Map<Church>(churchdto);
+            if (id != church.Id || !await _service.MeetsTheConstraints(church))
+            {
+                return BadRequest();
+            }
+
+            await _service.UpdateAsync(church);
+            return Ok(_mapper.Map<ChurchDTO>(church));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ChurchDTO>> PostChurch(ChurchDTO churchdto)
+        {
+            var church = _mapper.Map<Church>(churchdto);
+            if (!await _service.MeetsTheConstraints(church))
+            {
+                return BadRequest();
+            }
+
+            await _service.AddAsync(church);
+            return CreatedAtAction(nameof(GetChurch), new { id = church.Id }, _mapper.Map<ChurchDTO>(church));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteChurch(int id)
+        {
+            if (!await _service.EntityExists(id))
+            {
+                return NotFound();
+            }
+
+            await _service.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
