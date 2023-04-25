1fb080e40749c5b27ff0a798cd9e9f23dd767312
lytwp8 <lytwp8@pte.hu>
2023-04-25 19:31:42 +0200

    Add Devtunnel and Swagger to the project

--- KatifiWebServer/Program.cs
+++ KatifiWebServer/Program.cs
@@ -1,4 +1,5 @@
 using KatifiWebServer.Data;
+using KatifiWebServer.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,14 +11,47 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<MicrosoftEFContext>(opt => opt.UseSqlServer(
     builder.Configuration.GetConnectionString("LocalConnection")));
 
+builder.Services.AddAutoMapper(typeof(MapperConfig));
+builder.Services.AddScoped<MicrosoftEFContext>();
+
+builder.Services.AddScoped<IAddressService, AddressService>();
+builder.Services.AddScoped<IChurchService, ChurchService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ICommunityService, CommunityService>();
+builder.Services.AddScoped<IMessService, MessService>();
+builder.Services.AddScoped<IEventService, EventService>();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Title = "API testing",
+        Version = "v1"
+    });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+IWebHostEnvironment env = app.Services.GetRequiredService<IWebHostEnvironment>();
+if (env.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API testing");
+    });
+}
+
+DBInitializer.Seed(app);
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+Console.WriteLine($"Tunnel URL: {Environment.GetEnvironmentVariable("VS_TUNNEL_URL")}");
