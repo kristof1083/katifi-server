be350f2fb0f59b6286daf9989e4317fe092a1501
lytwp8 <lytwp8@pte.hu>
2023-04-25 19:41:21 +0200

    Create UserController.cs

---
+++ KatifiWebServer/Controllers/UserController.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using KatifiWebServer.Models.DatabaseModels;
+using KatifiWebServer.Models.DTOModels;
+using KatifiWebServer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KatifiWebServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _service;
+        private readonly IMapper _mapper;
+
+        public UserController(IUserService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserDTO>>> GetUsers()
+        {
+            var users = await _service.GetAllAsync(u => u.Address, u => u.Role);
+            var userdtos = _mapper.Map<IEnumerable<UserDTO>>(users);
+
+            return Ok(userdtos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDTO>> GetUser(int id)
+        {
+            var user = await _service.GetByIdAsync(id, u => u.Address, u=>u.Role);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<UserDTO>(user));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UserDTO>> PutUser(int id, UserDTO userdto)
+        {
+            var user = _mapper.Map<User>(userdto);
+            if (id != user.Id || !await _service.MeetsTheConstraints(user))
+            {
+                return BadRequest();
+            }
+
+            await _service.UpdateAsync(user);
+            return Ok(_mapper.Map<UserDTO>(user));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserDTO>> PostUser(UserDTO userdto)
+        {
+            var user = _mapper.Map<User>(userdto);
+            if (!await _service.MeetsTheConstraints(user))
+            {
+                return BadRequest();
+            }
+
+            await _service.AddAsync(user);
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, _mapper.Map<UserDTO>(user));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            if (!await _service.EntityExists(id))
+            {
+                return NotFound();
+            }
+
+            await _service.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
