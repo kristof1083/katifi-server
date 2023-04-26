b849be38293089665d63eee66ad769050ab644de
lytwp8 <lytwp8@pte.hu>
2023-04-26 21:06:04 +0200

    Create Security models

---
+++ KatifiWebServer/Models/SecurityModels/RegisterModel.cs
@@ -0,0 +1,35 @@
+﻿using KatifiWebServer.Models.DTOModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace KatifiWebServer.Models.SecurityModels
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Lastname is required")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "FirstName is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Binary gender selection is required")]
+        public char Gender { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        public DateOnly BornDate { get; set; }
+
+        [Required(ErrorMessage = "You have to agree to the end user's terms")]
+        public bool AgreeTerm { get; set; }
+
+        public string? UserToken { get; set; }
+
+        public int? AddressID { get; set; }
+    }
+}
---
+++ KatifiWebServer/Models/SecurityModels/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace KatifiWebServer.Models.SecurityModels
+{
+    public class Response
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
