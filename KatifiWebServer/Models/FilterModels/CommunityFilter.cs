c02a25ff0831f4e077eb9234a69eb67155200c7c
lytwp8 <lytwp8@pte.hu>
2023-05-12 11:59:13 +0200

    Add filters for Community and Event models to make consist requests from controllers to services

---
+++ KatifiWebServer/Models/FilterModels/CommunityFilter.cs
@@ -0,0 +1,11 @@
+﻿namespace KatifiWebServer.Models.FilterModels
+{
+    public class CommunityFilter
+    {
+        public string? CountryCode { get; set; } = string.Empty;
+        public string? City { get; set; } = string.Empty;
+        public bool JustOpens { get; set; } = false;
+        public string? CommunityNameSubstring { get; set; } = string.Empty;
+        public int MinimumMember { get; set; } = 0;
+    }
+}
---
+++ KatifiWebServer/Models/FilterModels/EventFilter.cs
@@ -0,0 +1,15 @@
+﻿namespace KatifiWebServer.Models.FilterModels
+{
+    public class EventFilter
+    {
+        public string? CountryCode { get; set; } = string.Empty;
+        public string? City { get; set; } = string.Empty;
+        public bool JustActives { get; set; } = false;
+        public string? EventNameSubstring { get; set; } = string.Empty;
+        public int MinimumParticipant { get; set; } = 0;
+
+        public DateTime? EarliestEventDate { get; set; } = DateTime.MinValue;
+        public DateTime? LatestEventDate { get; set; } = DateTime.MinValue;
+
+    }
+}
