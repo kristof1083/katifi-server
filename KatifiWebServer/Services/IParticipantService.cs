26d698e4c222d2e83766f44668405f42bcd961a6
lytwp8 <lytwp8@pte.hu>
2023-05-12 11:52:42 +0200

    Add IParticipantService.cs with four methods and implement all.

---
+++ KatifiWebServer/Services/IParticipantService.cs
@@ -0,0 +1,13 @@
+﻿using KatifiWebServer.Data.Base;
+using KatifiWebServer.Models.DatabaseModels;
+
+namespace KatifiWebServer.Services
+{
+    public interface IParticipantService : IEntityBaseRepository<Participant>
+    {
+        public Task<Participant?> GetByIdsAsync(int eventId, int userId);
+        public Task<IEnumerable<int>> GetEventIdsByUserID(int userId);
+        public Task<IEnumerable<Participant>> GetParticipantsAsync(int eventId);
+        public Task<bool> UserIsRegistred(int eventId, int userId);
+    }
+}
---
+++ KatifiWebServer/Services/ParticipantService.cs
@@ -0,0 +1,42 @@
+﻿using KatifiWebServer.Data;
+using KatifiWebServer.Data.Base;
+using KatifiWebServer.Models.DatabaseModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace KatifiWebServer.Services
+{
+    public class ParticipantService : EntityBaseRepository<Participant>, IParticipantService
+    {
+        private readonly MicrosoftEFContext _context;
+
+        public ParticipantService(MicrosoftEFContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Participant?> GetByIdsAsync(int eventId, int userId)
+        {
+            var q = _context.Participants.Include(p => p.Event).Include(p => p.User).Where(p => p.EventId == eventId && p.UserId == userId);
+            return await q.FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<int>> GetEventIdsByUserID(int userId)
+        {
+            return await _context.Participants.AsNoTracking().Where(p => p.UserId == userId).Select(p=>p.EventId).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Participant>> GetParticipantsAsync(int eventId)
+        {
+            return await _context.Participants.Include(p => p.Event).Include(p => p.User).ThenInclude(u => u.Address).Where(p => p.EventId == eventId).ToListAsync();
+        }
+
+        public async Task<bool> UserIsRegistred(int eventId, int userId)
+        {
+            int partid = await _context.Participants.AsNoTracking().Where(p => p.EventId == eventId && p.UserId == userId).Select(p => p.Id).FirstOrDefaultAsync();
+            if (partid > 0)
+                return true;
+
+            return false;
+        }
+    }
+}
