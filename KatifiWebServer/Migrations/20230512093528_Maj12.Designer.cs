fee0a9e11a6ecb4ac8a7ca6a226e2074564ad162
lytwp8 <lytwp8@pte.hu>
2023-05-12 11:38:07 +0200

    Update migration: Migrations/20230512093528_Maj12.cs

--- KatifiWebServer/Migrations/20230512093528_Maj12.Designer.cs
+++ KatifiWebServer/Migrations/20230512093528_Maj12.Designer.cs
@@ -4,6 +4,7 @@ using KatifiWebServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KatifiWebServer.Migrations
 {
     [DbContext(typeof(MicrosoftEFContext))]
-    partial class MicrosoftEFContextModelSnapshot : ModelSnapshot
+    [Migration("20230512093528_Maj12")]
+    partial class Maj12
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
