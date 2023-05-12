7500bc08dec81f7b383985dca1e35ac686faaadb
lytwp8 <lytwp8@pte.hu>
2023-05-12 11:45:52 +0200

    Add 5 method to IAddressService and implement them

--- KatifiWebServer/Services/AddressService.cs
+++ KatifiWebServer/Services/AddressService.cs
@@ -1,26 +1,125 @@
 ﻿using KatifiWebServer.Data;
 using KatifiWebServer.Data.Base;
 using KatifiWebServer.Models.DatabaseModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace KatifiWebServer.Services
 {
     public class AddressService : EntityBaseRepository<Address>, IAddressService
     {
+        private readonly MicrosoftEFContext _context;
+
         public AddressService(MicrosoftEFContext context) : base(context)
         {
+            _context = context;
         }
 
-        public override async Task<bool> MeetsTheConstraints(Address address)
+        public override bool MeetsTheConstraints(Address address)
         {
-            bool basebool = await base.MeetsTheConstraints(address);
+            bool basebool = base.MeetsTheConstraints(address);
             if (basebool && !string.IsNullOrWhiteSpace(address.CountryCode) && address.CountryCode.Length <= 3 && !string.IsNullOrWhiteSpace(address.County) && !string.IsNullOrWhiteSpace(address.City))
             {
-                if(address.PostCode == null || (address.PostCode != null && address.PostCode >= 1000 && address.PostCode < 10000))
+                if (address.PostCode == null || (address.PostCode != null && address.PostCode >= 1000 && address.PostCode < 10000))
                 {
-                    return await Task.FromResult(true);
+                    return true;
                 }
             }
-            return await Task.FromResult(false);
+            return false;
+        }
+
+
+        public async Task<int> EntityExists(Address address)
+        {
+            var existingaddress = await _context.Set<Address>()
+                .AsNoTracking()
+                .Where(a => a.CountryCode == address.CountryCode && a.County == address.County && a.City == address.City &&
+                    a.Street == address.Street && a.PostCode == address.PostCode && a.HouseNumber == address.HouseNumber)
+                .FirstOrDefaultAsync();
+
+            if (existingaddress == null)
+                return -1;
+
+            return existingaddress.Id;
+        }
+
+        public async Task<int> ConnectedEntitysNumber(int id)
+        {
+            try
+            {
+                if (id == 0)
+                    return 0;
+
+                var address = await _context.Addresses
+                .AsNoTracking()
+                .Include(a => a.Users).Include(a => a.Events).Include(a => a.Communities).Include(a => a.Church)
+                .Where(a => a.Id == id)
+                .FirstOrDefaultAsync();
+
+                if (address == null)
+                    return 0;
+
+                int count = address.Users.Count + address.Events.Count + address.Communities.Count;
+                if (address.Church != null)
+                    count++;
+
+                return count;
+            }
+            catch (Exception ex)
+            {
+                var m = ex.Message;
+                return 0;
+            }
+        }
+
+        public async Task<bool> HasChurchInLocation(int addressid)
+        {
+            var address = await _context.Addresses.AsNoTracking().Include(a => a.Church).Where(a=>a.Id == addressid).FirstOrDefaultAsync();
+            
+            if(address == null || address.Church == null)
+                return false;
+
+            return true;
+        }
+
+        public async Task<int> HandleInternalAddressUpdate(Address address)
+        {
+            int pe_addressid = await this.EntityExists(address);
+            int cetoold = await this.ConnectedEntitysNumber(address.Id);
+            
+            if(address.Id != pe_addressid)
+            {
+                if (pe_addressid > 0)
+                {
+                    return pe_addressid;
+                }
+                else
+                {
+                    if(cetoold == 1)
+                    {
+                        await base.UpdateAsync(address);
+                        return address.Id;
+                    }
+                    else
+                    {
+                        address.Id = 0;
+                        await this.AddAsync(address);
+                        return address.Id;
+                    }
+                }
+            }
+
+            return address.Id;
+        }
+
+        public async Task DeleteUnusedAddresses()
+        {
+            foreach(var address in await _context.Addresses.ToListAsync())
+            {
+                if(await ConnectedEntitysNumber(address.Id) == 0)
+                {
+                    await base.DeleteAsync(address.Id);
+                }
+            }
         }
     }
 }
--- KatifiWebServer/Services/IAddressService.cs
+++ KatifiWebServer/Services/IAddressService.cs
@@ -5,6 +5,15 @@ namespace KatifiWebServer.Services
 {
     public interface IAddressService : IEntityBaseRepository<Address>
     {
+        public Task<int> EntityExists(Address address);
+
+        public Task<int> ConnectedEntitysNumber(int id);
+
+        public Task<int> HandleInternalAddressUpdate(Address possNewAddress);
+
+        public Task<bool> HasChurchInLocation(int addressid);
+
+        public Task DeleteUnusedAddresses();
     }
         
 }
