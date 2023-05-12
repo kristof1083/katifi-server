ccb5c3eb4d5c1de4c5d9220db905805d110fcaf3
lytwp8 <lytwp8@pte.hu>
2023-05-12 11:48:22 +0200

    Add 2 methods to IEventService and implement them.

--- KatifiWebServer/Services/EventService.cs
+++ KatifiWebServer/Services/EventService.cs
@@ -1,24 +1,74 @@
 ﻿using KatifiWebServer.Data;
 using KatifiWebServer.Data.Base;
 using KatifiWebServer.Models.DatabaseModels;
+using KatifiWebServer.Models.FilterModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace KatifiWebServer.Services
 {
     public class EventService : EntityBaseRepository<Event>, IEventService
     {
+        private readonly MicrosoftEFContext _context;
+
         public EventService(MicrosoftEFContext context) : base(context)
         {
+            _context = context;
         }
 
-        public override async Task<bool> MeetsTheConstraints(Event eve)
+        public override bool MeetsTheConstraints(Event eve)
         {
-            bool basebool = await base.MeetsTheConstraints(eve);
+            bool basebool = base.MeetsTheConstraints(eve);
             if (basebool && !string.IsNullOrWhiteSpace(eve.Name) && !string.IsNullOrWhiteSpace(eve.Organizer) &&
                 eve.Date != DateTime.MinValue && eve.RegistrationDeadline != DateTime.MinValue && eve.Date >= eve.RegistrationDeadline)
             {
-                return await Task.FromResult(true);
+                return true;
             }
-            return await Task.FromResult(false);
+            return false;
         }
+
+        public async Task<IEnumerable<Event>> GetAllEventAsync()
+        {
+            var q = _context.Events.Include(e=>e.Address).Include(e => e.Participants).ThenInclude(p => p.User);
+            return await q.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Event>> GetFilteredEventsAsync(EventFilter filter)
+        {
+            IQueryable<Event> query = _context.Events.Include(e => e.Address).Include(e => e.Participants).ThenInclude(p => p.User);
+
+            if(filter.EarliestEventDate == null)
+                filter.EarliestEventDate = DateTime.MinValue;
+
+            if (filter.LatestEventDate == null || filter.LatestEventDate == DateTime.MinValue)
+                filter.LatestEventDate = DateTime.MaxValue;
+
+            query = query.Where(e => e.Date.Date >= filter.EarliestEventDate.Value.Date && e.Date.Date <= filter.LatestEventDate.Value.Date);
+
+
+            if (!string.IsNullOrWhiteSpace(filter.CountryCode))
+            {
+                query = query.Where(e => e.Address.CountryCode.ToLower() == filter.CountryCode.ToLower());
+            }
+            if (!string.IsNullOrWhiteSpace(filter.City))
+            {
+                query = query.Where(e => e.Address.City.ToLower() == filter.City.ToLower());
+            }
+            if (!string.IsNullOrWhiteSpace(filter.EventNameSubstring))
+            {
+                query = query.Where(e => e.Name.ToLower().Contains(filter.EventNameSubstring.ToLower()));
+            }
+            if (filter.JustActives)
+            {
+                query = query.Where(e => e.Date.Date >= DateTime.Now.Date);
+            }
+            if (filter.MinimumParticipant > 0)
+            {
+                query = query.Where(e => e.Participants.Count() >= filter.MinimumParticipant);
+            }
+
+            return await query.ToListAsync();
+        }
+
+
     }
 }
--- KatifiWebServer/Services/IEventService.cs
+++ KatifiWebServer/Services/IEventService.cs
@@ -1,9 +1,12 @@
 ﻿using KatifiWebServer.Data.Base;
 using KatifiWebServer.Models.DatabaseModels;
+using KatifiWebServer.Models.FilterModels;
 
 namespace KatifiWebServer.Services
 {
     public interface IEventService : IEntityBaseRepository<Event>
     {
+        public Task<IEnumerable<Event>> GetAllEventAsync();
+        public Task<IEnumerable<Event>> GetFilteredEventsAsync(EventFilter filter);
     }
 }
