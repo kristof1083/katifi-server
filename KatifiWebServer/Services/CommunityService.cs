f779d7ee34e618e3b33d90ae00e1cd804f69246b
lytwp8 <lytwp8@pte.hu>
2023-05-12 11:54:55 +0200

    Add 1 method to ICommunityService and implement it.  - GetFilteredCommunitiesAsync

--- KatifiWebServer/Services/CommunityService.cs
+++ KatifiWebServer/Services/CommunityService.cs
@@ -1,8 +1,8 @@
 ﻿using KatifiWebServer.Data;
 using KatifiWebServer.Data.Base;
 using KatifiWebServer.Models.DatabaseModels;
+using KatifiWebServer.Models.FilterModels;
 using Microsoft.EntityFrameworkCore;
-using System.Linq.Expressions;
 
 namespace KatifiWebServer.Services
 {
@@ -15,20 +15,48 @@ namespace KatifiWebServer.Services
             _context = context;
         }
 
-        public override async Task<bool> MeetsTheConstraints(Community com)
+        public override bool MeetsTheConstraints(Community com)
         {
-            bool basebool = await base.MeetsTheConstraints(com);
-            if (basebool && !string.IsNullOrWhiteSpace(com.Name) && com.AddressId >= 338)
+            bool basebool = base.MeetsTheConstraints(com);
+            if (basebool && !string.IsNullOrWhiteSpace(com.Name))
             {
-                return await Task.FromResult(true);
+                return true;
             }
-            return await Task.FromResult(false);
+            return false;
         }
 
         public async Task<IEnumerable<Community>> GetAllCommunityAsync()
         {
             var q = _context.Communities.Include(c => c.Address).Include(c=>c.Members).ThenInclude(m=>m.User);
             return await q.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Community>> GetFilteredCommunitiesAsync(CommunityFilter filter)
+        {
+            IQueryable<Community> query = _context.Communities.Include(c => c.Address).Include(c => c.Members).ThenInclude(m => m.User);
+            
+            if (!string.IsNullOrWhiteSpace(filter.CountryCode))
+            {
+                query = query.Where(c => c.Address.CountryCode.ToLower() == filter.CountryCode.ToLower());
+            }
+            if (!string.IsNullOrWhiteSpace(filter.City))
+            {
+                query = query.Where(c => c.Address.City.ToLower() == filter.City.ToLower());
+            }
+            if (filter.JustOpens)
+            {
+                query = query.Where(c => c.IsOpen == true);
+            }
+            if (!string.IsNullOrWhiteSpace(filter.CommunityNameSubstring))
+            {
+                query = query.Where(c => c.Name.ToLower().Contains(filter.CommunityNameSubstring.ToLower()));
+            }
+            if (filter.MinimumMember > 0)
+            {
+                query = query.Where(c => c.Members.Count() >= filter.MinimumMember);
+            }
+
+            return await query.ToListAsync();
         }
     }
 }
--- KatifiWebServer/Services/ICommunityService.cs
+++ KatifiWebServer/Services/ICommunityService.cs
@@ -1,10 +1,13 @@
 ﻿using KatifiWebServer.Data.Base;
 using KatifiWebServer.Models.DatabaseModels;
+using KatifiWebServer.Models.FilterModels;
 
 namespace KatifiWebServer.Services
 {
     public interface ICommunityService : IEntityBaseRepository<Community>
     {
         public Task<IEnumerable<Community>> GetAllCommunityAsync();
+
+        public Task<IEnumerable<Community>> GetFilteredCommunitiesAsync(CommunityFilter filter);
     }
 }
