d817857aac5129048509c82742d79b59707552fb
lytwp8 <lytwp8@pte.hu>
2023-05-12 11:49:51 +0200

    Adapt MapperConfig to Model changes.

--- KatifiWebServer/Data/MapperConfig.cs
+++ KatifiWebServer/Data/MapperConfig.cs
@@ -2,7 +2,6 @@
 using KatifiWebServer.Models.DatabaseModels;
 using KatifiWebServer.Models.DTOModels;
 using KatifiWebServer.Models.SecurityModels;
-using Microsoft.Build.Framework;
 
 namespace KatifiWebServer.Data;
 
@@ -14,32 +13,34 @@ public class MapperConfig : Profile
         CreateMap<Address, AddressDTO>().ReverseMap();
 
         CreateMap<Church, ChurchDTO>();
-        CreateMap<Church, ChurchDTO>().ReverseMap();
+        CreateMap<Church, ChurchDTO>().ReverseMap()
+            .ForMember(dest => dest.AddressId, opt => opt.MapFrom(src => src.Address.Id));
 
         CreateMap<Mess, MessDTO>();
         CreateMap<Mess, MessDTO>().ReverseMap();
 
         CreateMap<Community, CommunityDTO>();
-        CreateMap<Community, CommunityDTO>().ReverseMap();
+        CreateMap<Community, CommunityDTO>().ReverseMap()
+            .ForMember(dest => dest.AddressId, opt => opt.MapFrom(src => src.Address.Id))
+            .ForMember(dest => dest.MemberCount, opt => opt.Ignore());
+
+        CreateMap<Member, MemberDTO>()
+            .ForMember(dest => dest.UserFullName, opt => opt.MapFrom(src => src.User.FullName))
+            .ForMember(dest => dest.UserAge, opt => opt.MapFrom(src => src.User.Age));
+        CreateMap<Member, MemberDTO>().ReverseMap();
+
+        CreateMap<Event, EventDTO>();
+        CreateMap<Event, EventDTO>().ReverseMap()
+            .ForMember(dest => dest.AddressId, opt => opt.MapFrom(src => src.Address.Id))
+            .ForMember(dest => dest.ParticipantCount, opt => opt.Ignore());
+
+        CreateMap<Participant, ParticipantDTO>()
+            .ForMember(dest => dest.UserFullName, opt => opt.MapFrom(src => src.User.FullName))
+            .ForMember(dest => dest.UserAge, opt => opt.MapFrom(src => src.User.Age));
+        CreateMap<Participant, ParticipantDTO>().ReverseMap();
 
         CreateMap<AppUser, UserDTO>();
         CreateMap<AppUser, UserDTO>().ReverseMap();
-
-        CreateMap<Member, MemberDTO>()
-            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
-            .ForMember(dest => dest.CommunityName, opt => opt.MapFrom(src => src.Community.Name))
-            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.FullName));
-        CreateMap<Member, MemberDTO>()
-            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
-            .ForMember(dest => dest.CommunityName, opt => opt.MapFrom(src => src.Community.Name))
-            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.FullName)).ReverseMap();
-
-        CreateMap<Event, EventDTO>();
-        CreateMap<Event, EventDTO>().ReverseMap();
-
-        CreateMap<Participant, ParticipantDTO>();
-        CreateMap<Participant, ParticipantDTO>().ReverseMap();
-
 
         //Identity 
         CreateMap<RegisterModel, AppUser>();
