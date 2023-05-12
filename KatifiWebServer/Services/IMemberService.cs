d90b3c675606d1a325890e5bfd1c5444c4e8b2f6
lytwp8 <lytwp8@pte.hu>
2023-05-12 11:51:52 +0200

    Add IMemberService.cs with two methods and implement it.

---
+++ KatifiWebServer/Services/IMemberService.cs
@@ -0,0 +1,11 @@
+﻿using KatifiWebServer.Data.Base;
+using KatifiWebServer.Models.DatabaseModels;
+
+namespace KatifiWebServer.Services
+{
+    public interface IMemberService : IEntityBaseRepository<Member>
+    {
+        public Task<Member?> GetByIdsAsync(int communityId, int userId);
+        public Task<IEnumerable<Member>> GetMembersAsync(int communityId, bool justactiveones = false);
+    }
+}
---
+++ KatifiWebServer/Services/MemberService.cs
@@ -0,0 +1,35 @@
+﻿using KatifiWebServer.Data;
+using KatifiWebServer.Data.Base;
+using KatifiWebServer.Models.DatabaseModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace KatifiWebServer.Services
+{
+    public class MemberService : EntityBaseRepository<Member>, IMemberService
+    {
+        private readonly MicrosoftEFContext _context;
+
+        public MemberService(MicrosoftEFContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Member?> GetByIdsAsync(int communityId, int userId)
+        {
+            var q = _context.Members.Include(m => m.Community).Include(m => m.User).Where(m => m.CommunityId == communityId && m.UserId == userId);
+            return await q.FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Member>> GetMembersAsync(int communityId, bool justactiveones)
+        {
+            var q = _context.Members.Include(m => m.Community).Include(m => m.User).ThenInclude(u=>u.Address).Where(m=>m.CommunityId == communityId);
+
+            if (justactiveones)
+            {
+                q = q.Where(m => m.LeaveDate == null);
+            }
+
+            return await q.ToListAsync();
+        }
+    }
+}
